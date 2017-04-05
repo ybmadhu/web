d2aeb4eafa831f66191f6a224693212c566db827
ybmadhu <ybmadhu707@gmail.com>
2017-04-05 22:32:42 +0530

    ServletExample
    myservlet project

---
+++ ServletsExamples/src/com/msr/servlets/MyServlet.java
@@ -0,0 +1,56 @@
+package com.msr.servlets;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+@WebServlet("/MyServlet")
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+    public void init() throws ServletException
+    {
+          System.out.println("----------");
+          System.out.println("---------- CrunchifyServletExample Initialized successfully ----------");
+          System.out.println("----------");
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	PrintWriter out=response.getWriter();
+	out.print("Hello DevOps Gems");
+	
+		System.out.println("this is get");// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
